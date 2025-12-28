6fbf44e3b2599c1f739f8ff46045a428e49654c4
Anime Tano <animetano32@gmail.com>
2025-12-28 15:11:09 +0300

    update

--- include/User.h
+++ include/User.h
@@ -10,4 +10,12 @@ class User{
         int id;
 
     public:
+        User(const std::string& name, int i) : username(name), id(i) {}
+
+
+        std::string getUsername() const { return username; }
+        int getId() const { return id; }
+
+        void setUsername(const std::string& name) { username = name; }
+        void setId(int i) { id = i; }
 };
